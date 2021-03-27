55cc92d5639a50e0323d5daf0afb0238e7858bc4
Marx Roberto <marxmoita@gmail.com>
2021-03-27 14:08:54 -0300

    implementando todos os metodos do desafio

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ app.get("/repositories", (request, response) => {
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body
 
+  
   const repository = {
     id: uuid(),
     title,
@@ -23,20 +24,29 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository)
+  
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  console.log(updatedRepository);
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
+  updatedRepository.likes = repositories[repositoryIndex].likes;
+
+
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+
+  console.log(repository)
 
   repositories[repositoryIndex] = repository;
 
@@ -48,7 +58,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -68,7 +78,9 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  repositories[repositoryIndex].likes = likes;
+
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
